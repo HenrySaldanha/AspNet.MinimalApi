d0ce8468b7e1ba7e35da0dca97e39186f006660d
HenrySaldanha <henry.l.saldanha@gmail.com>
2022-08-05 19:14:16 -0300

    feat: add request and response entities

---
+++ Models/Request/CreateMovieRequest.cs
@@ -0,0 +1,23 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System.Text.Json.Serialization;
+
+public class CreateMovieRequest : Notifiable<Notification>
+{
+    public string Name { get; set; }
+    public DateTime Released { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public Genre Genre { get; set; }
+
+    public static implicit operator Movie(CreateMovieRequest request)
+    {
+        var contract = new Contract<Notification>()
+            .Requires()
+            .IsNotNull(request.Name, "Name is null")
+            .IsLowerOrEqualsThan(request.Released, DateTime.UtcNow, "Invalid date");
+
+        request.AddNotifications(contract);
+
+        return new Movie(Guid.NewGuid(), request.Name, request.Released, request.Genre);
+    }
+}
---
+++ Models/Response/MovieResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+public class MovieResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Released { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public Genre Genre { get; set; }
+
+    public static implicit operator MovieResponse(Movie movie)
+    {
+        if (movie is null) 
+            return null;
+
+        return new MovieResponse
+        {
+            Id = movie.Id,
+            Name = movie.Name,
+            Released = movie.Released,
+            Genre = movie.Genre,
+        };
+    }
+}
