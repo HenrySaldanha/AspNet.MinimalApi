3d34abf368ef28f134c7e2810c7bf2d4ffb55627
HenrySaldanha <henry.l.saldanha@gmail.com>
2022-08-05 19:15:15 -0300

    feat: add sql lite code

---
+++ Database/MovieDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class MovieDbContext : DbContext
+{
+    public DbSet<Movie> Movies { get; set; }
+
+    public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options) { }
+}
---
+++ Database/MovieRepository.cs
@@ -0,0 +1,28 @@
+﻿public class MovieRepository
+{
+    public MovieDbContext _dbContext { get; set; }
+
+    public MovieRepository(MovieDbContext movieDbContext)
+    {
+        _dbContext = movieDbContext;
+    }
+
+    public IEnumerable<Movie> Get() =>
+        _dbContext.Movies.ToArray();
+
+    public Movie Get(Guid id) =>
+        _dbContext.Movies.FirstOrDefault(c => c.Id == id);
+
+    public Movie Create(Movie movie)
+    {
+        _dbContext.Movies.Add(movie);
+        _dbContext.SaveChanges();
+        return movie;
+    }
+
+    public void Delete(Movie movie)
+    {
+        _dbContext.Movies.Remove(movie);
+        _dbContext.SaveChanges();
+    }
+}
